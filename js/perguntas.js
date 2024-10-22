bcd6601852b6e79eef2ab9b7529588cf47f720eb
macielcrf <128715902+macielcrf@users.noreply.github.com>
2024-10-22 08:00:13 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,82 @@
+export const perguntas = [
+    {
+        enunciado: "Qual o menor país do mundo em extensão territorial",
+        alternativas: [
+            {
+                texto: "Vaticano",
+                afirmacao: [
+                    "Vaticano é o menor país do mundo em extensão territorial"
+                    ],
+                proxima: 1,
+            },
+            {
+                texto: "Nauru",
+                afirmacao: [
+                    "Errado, é o vaticano"
+                    ],
+                proxima: 1, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "qual o maior oceano do mundo em área?",
+        alternativas: [
+            {
+                texto:"Pacífico",
+                afirmacao: [
+                    "Correto"
+                    ],
+                proxima: 2, 
+            },
+            {
+                texto: "Atlântico",
+                afirmacao: [
+                    "Errado, é o pacifíco"
+                    ],
+                proxima: 2, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Qual o maior deserto do mundo?",
+        alternativas: [
+            {
+                texto:"Deserto do Saara",
+                afirmacao: [
+                    "Errado, o maior deserto do mundo fica na antartica, o saara é o maior deserto QUENTE"
+                    ],
+                proxima: 3, 
+            },
+            {
+                texto:"Antartica",
+                afirmacao: [
+                    "Correto"
+                ],
+                proxima: 3, 
+            },
+        ]
+    },
+    
+    {
+        enunciado: "Qual é o país com a maior área de floresta",
+        alternativas: [
+            {
+                texto: "Russia, floresta de taiga",
+                afirmacao: [
+                    ""
+                    ],
+                proxima: 4, 
+            },
+            {
+                texto: "Brasil, floresta amazônica",
+                afirmacao: [
+                    ""
+                ],
+                proxima: 4, 
+            },     
+        ]
+    },
+    
+];
